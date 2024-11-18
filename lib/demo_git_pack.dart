b272f0bca7c72eeb568b3ab8e83c879af2b65fec
Raghib Arshi <46123959+RaghibArshi@users.noreply.github.com>
2024-11-18 14:47:47 +0530

    Update demo_git_pack.dart

--- lib/demo_git_pack.dart
+++ lib/demo_git_pack.dart
@@ -2,7 +2,5 @@ library demo_git_pack;
 
 /// A Calculator.
 class Calculator {
-  int value = 0;
-  value = value + 1;
-  var data = 'Print_Flutter_Demo_GIT_Pack:- '+ value.toString();
+  String data = 'Print_Flutter_Demo_GIT_Pack:-';
 }
