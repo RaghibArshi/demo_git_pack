bb79a1368da48cd34e78fa036fe5326a4ccee766
Raghib Arshi <46123959+RaghibArshi@users.noreply.github.com>
2024-11-25 15:08:11 +0530

    Add files via upload

---
+++ lib/bo_shift_api.dart
@@ -0,0 +1,112 @@
+part of swagger.api;
+
+
+
+class BOShiftApi {
+  final ApiClient apiClient;
+
+  BOShiftApi([ApiClient apiClient]) : apiClient = apiClient ?? defaultApiClient;
+
+  /// CreateShiftForBOHandler inserts an entry for Counter Allocation &amp; Shift Begin for a BO in the database
+  ///
+  /// Inserts an entry for Counter Allocation &amp; Shift Begin for a BO
+  Future<ResponseCtrAllocReportAPIResponse> boShiftsBeginPost(HandlerBeginShiftWithoutCounterAllocation body) async {
+    Object postBody = body;
+
+    // verify required params are set
+    if(body == null) {
+     throw new ApiException(400, "Missing required param: body");
+    }
+
+    // create path and map variables
+    String path = "/bo-shifts/begin".replaceAll("{format}","json");
+
+    // query params
+    List<QueryParam> queryParams = [];
+    Map<String, String> headerParams = {};
+    Map<String, String> formParams = {};
+    
+    List<String> contentTypes = ["application/json"];
+
+    String contentType = contentTypes.length > 0 ? contentTypes[0] : "application/json";
+    List<String> authNames = [];
+
+    if(contentType.startsWith("multipart/form-data")) {
+      bool hasFields = false;
+      MultipartRequest mp = new MultipartRequest(null, null);
+      if(hasFields)
+        postBody = mp;
+    }
+    else {
+          }
+
+    var response = await apiClient.invokeAPI(path,
+                                             'POST',
+                                             queryParams,
+                                             postBody,
+                                             headerParams,
+                                             formParams,
+                                             contentType,
+                                             authNames);
+
+    if(response.statusCode >= 400) {
+      throw new ApiException(response.statusCode, response.body);
+    } else if(response.body != null) {
+      return
+          apiClient.deserialize(response.body, 'ResponseCtrAllocReportAPIResponse') as ResponseCtrAllocReportAPIResponse ;
+    } else {
+      return null;
+    }
+  }
+  /// UpdateShiftEndForBOHandler updates the BO shift end details
+  ///
+  /// Updates the BO shift end details
+  Future<ResponseCtrShiftEndAPIResponse> boShiftsMarkEndPut(HandlerUpdateBOShift body) async {
+    Object postBody = body;
+
+    // verify required params are set
+    if(body == null) {
+     throw new ApiException(400, "Missing required param: body");
+    }
+
+    // create path and map variables
+    String path = "/bo-shifts/mark-end".replaceAll("{format}","json");
+
+    // query params
+    List<QueryParam> queryParams = [];
+    Map<String, String> headerParams = {};
+    Map<String, String> formParams = {};
+    
+    List<String> contentTypes = ["application/json"];
+
+    String contentType = contentTypes.length > 0 ? contentTypes[0] : "application/json";
+    List<String> authNames = [];
+
+    if(contentType.startsWith("multipart/form-data")) {
+      bool hasFields = false;
+      MultipartRequest mp = new MultipartRequest(null, null);
+      if(hasFields)
+        postBody = mp;
+    }
+    else {
+          }
+
+    var response = await apiClient.invokeAPI(path,
+                                             'PUT',
+                                             queryParams,
+                                             postBody,
+                                             headerParams,
+                                             formParams,
+                                             contentType,
+                                             authNames);
+
+    if(response.statusCode >= 400) {
+      throw new ApiException(response.statusCode, response.body);
+    } else if(response.body != null) {
+      return
+          apiClient.deserialize(response.body, 'ResponseCtrShiftEndAPIResponse') as ResponseCtrShiftEndAPIResponse ;
+    } else {
+      return null;
+    }
+  }
+}
